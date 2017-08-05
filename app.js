945333cf95de770bd1b510f5a664beff626d5bd9
Brittany <brithamilton1@gmail.com>
2017-08-05 14:50:45 -0400

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var app = angular.module("test", []);
 app.controller("AppCtrl", function ($scope){
   $scope.projects =[
-    {name: "project 1", image: "https://yt3.ggpht.com/EdjnobpzppDl5pSVU2s2AUIiFS0qBfT8Jdodw-FHMhugJK5zmzWDLkpqDVtpnaLSP66M5F8nqINImLKGtQ=s900-nd-c-c0xffffffff-rj-k-no", date: "06.23.2017", collaborators: null},
-    {name: "project 2", image: "http://placehold.it/200X200", date: "07.31.2017", collaborators: "Jessica"},
-    {name: "project 3", image: "http://placehold.it/200X200", date: "08.03.2017", collaborators: "Brittany and Jessica"}
+    {name: "project 1", image: "https://yt3.ggpht.com/EdjnobpzppDl5pSVU2s2AUIiFS0qBfT8Jdodw-FHMhugJK5zmzWDLkpqDVtpnaLSP66M5F8nqINImLKGtQ=s900-nd-c-c0xffffffff-rj-k-no", date: "06.23.2017", collaborators: null, caption: "Caption This"},
+    {name: "project 2", image: "http://placehold.it/200X200", date: "07.31.2017", collaborators: "Jessica", caption: "Caption This"},
+    {name: "project 3", image: "http://placehold.it/200X200", date: "08.03.2017", collaborators: "Brittany and Jessica", caption: "Caption This"}
   ]
 });
 
@@ -14,8 +14,13 @@ app.directive("isoScope", function (){
     },
     //this scope object makes it IsoScope
    templateUrl:"project.html",
-   restrict: "E"
+   restrict: "E",
+   link: function(scope, elem, attrs) {
+    elem.on("click", function(){
+    alert(scope.project.caption);
+    })
   }
+}
 })
 
 
